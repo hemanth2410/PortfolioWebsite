982083a8c3ac50fa4cefdcf0c8a0dc2f58e58503
hemanth sattenapalli <saihemanth.s@outlook.com>
2026-09-08 16:18:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+﻿const canvas = document.getElementById('Matrix');
+const context = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const katakana = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
+const russian = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдежзийклмнопрстуфхцчшщъыьэюяЁёІіЇїЄєҐґЉљЊњЋћЂђЏџ";
+const greek = 'ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩαβγδεζηθικλμνξοπρστυφχψω';
+const latin = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const korean = 'ㄱㄲㄴㄷㄸㄹㅁㅂㅃㅅㅆㅇㅈㅉㅊㅋㅌㅍㅎㅏㅐㅑㅒㅓㅔㅕㅖㅗㅘㅙㅚㅛㅜㅝㅞㅟㅠㅡㅢㅣ';
+const arabic = 'ابتثجحخدذرزسشصضطظعغفقكلمنهويءآأؤإئىة';
+const hebrew = 'אבגדהוזחטיכלמנסעפצקרשתךםןףץ';
+const nums = '0123456789';
+
+const alphadet = katakana + latin + nums + russian + greek + latin + korean + arabic + hebrew;
+
+const fontSize = 16;
+const columns = canvas.width / fontSize;
+const rainDrops = [];
+for (let x = 0; x < columns; x++) {
+    rainDrops[x] = 1;
+}
+const draw = () => {
+    context.fillStyle = 'rgba(0, 0, 0, 0.05)';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = '#0A0';
+    context.font = fontSize + 'px monospace';
+    for (let i = 0; i < rainDrops.length; i++) {
+        const text = alphadet.charAt(Math.floor(Math.random() * alphadet.length));
+        context.fillText(text, i * fontSize, rainDrops[i] * fontSize);
+        if (rainDrops[i] * fontSize > canvas.height && Math.random() > 0.975) {
+            rainDrops[i] = 0;
+        }
+        rainDrops[i]++;
+    }
+};
+
+setInterval(draw, 30);
